4ed6b65c15f3ba6f95906a807fad504b47692719
Amit Madhoriya <amit@constanci.com>
2021-12-02 22:23:36 +0000

    Updated index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const fs = require('fs');
 
 try {
   // `who-to-greet` input defined in action metadata file
@@ -8,8 +9,9 @@ try {
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+  /*const payload = JSON.stringify(github.context.payload, undefined, 2)
+  console.log(`The event payload: ${payload}`);*/
+  console.log(github.context.repo);
 } catch (error) {
   core.setFailed(error.message);
 }
