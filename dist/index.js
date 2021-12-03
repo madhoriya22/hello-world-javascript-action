13ecbf49ab0f998655e8abccfdad15c852f84503
Amit Madhoriya <amit@constanci.com>
2021-12-03 22:08:43 +0000

    log workspace

--- dist/index.js
+++ dist/index.js
@@ -8455,7 +8455,7 @@ try {
   // Get the JSON webhook payload for the event that triggered the workflow
   /*const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);*/
-  console.log(github.context.repo);
+  console.log(github.workspace);
 } catch (error) {
   core.setFailed(error.message);
 }
