16d78ef7ea06d763e322a32e79b1df922d035859
Amit Madhoriya <amit@constanci.com>
2021-12-05 19:08:27 +0000

    Updated code to read source destination jsons

--- dist/index.js
+++ dist/index.js
@@ -8455,11 +8455,19 @@ try {
   // Get the JSON webhook payload for the event that triggered the workflow
   /*const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);*/
-  fs.readFile(baseDirectory + '/source.json', function(err, data) {
+  fs.readFile(baseDirectory + '/source.json', (err, data) => {
     let sourceJson = JSON.parse(data);
     console.log('sourceJson -> '+sourceJson);
     console.log('data -> '+data);
-  })
+  });
+
+  fs.readFile(baseDirectory + '/destination.json', (err, data) => {
+    let destinationJson = JSON.parse(data);
+    console.log('destinationJson -> '+destinationJson);
+    console.log('data -> '+data);
+  });
+
+  
 } catch (error) {
   core.setFailed(error.message);
 }
